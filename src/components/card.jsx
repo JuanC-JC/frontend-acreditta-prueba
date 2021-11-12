04722c015a6fd947bfc175e951d782d2462437f2
JuanCJC <juancchaparro97@gmail.com>
2021-11-12 04:22:06 -0500

    [ADD] status good and bad card

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -4,14 +4,12 @@ import '../styles/card.scss'
 
 export default function Card ({data}) {
 
-
-  // console.log(data.img.url)
     return (
     <div className='card'>
       <img src={data.image.url} alt="" />
       <div className="card__info">
         <div className='card__name'>{data.name}</div>
-        <div className='card__status'>{data.biography.alignment}</div>
+        <div className={`card__status ${data.biography.alignment}`}></div>
       </div>
     </div>
 );
